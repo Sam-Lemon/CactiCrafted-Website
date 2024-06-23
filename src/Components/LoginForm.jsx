11923bd847ccda990b364b47aa8e89a5f2025ce9
Sam-Lemon <theatrewhisper@gmail.com>
2024-06-23 04:09:38 -0700

    mostly finished, input fields don't clear out correctly

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,18 +1,43 @@
+/** NOTES
+ * 
+ * email and password fields don't clear out correctly after submit
+ * need to put in the toggle logic back and forth from registration
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
-export default function LoginForm () {
+export default function LoginForm (props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        //Resets email and password field after submission.
+        setEmail("");
+        setPassword("");
+
+    };
 
     return(
-        <Form className='login-form'>
+        <Form className='login-form' onSubmit={handleSubmit}>
+            <h3 className='form-header'>Login</h3>
             <Form.Group className='mb-3' controlId='formGroupEmail'>
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type='email' placeholder='Enter Email' />
+                <Form.Control type='email' placeholder='Enter Email' onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group className='mb-3' controlId='formGroupPassword'>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Enter Password' />
+                <Form.Control type='password' placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
 
             <Form.Group className='mb-3' controlId='formBasicCheckbox'>
@@ -21,6 +46,10 @@ export default function LoginForm () {
 
             <Button variant="success" type="submit">
                 Log In
+            </Button>
+
+            <Button variant='link'>
+                Don't have an account? Register here.
             </Button>
         </Form>
     );
