37a16f52c022f6fd8146cb5b3738459173a834df
Sam-Lemon <theatrewhisper@gmail.com>
2024-07-02 16:29:53 -0700

    creating and styling

---
+++ src/Components/BrandBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BrandBanner () {
+
+    return(
+        <>
+        <div className="brand-box">
+            <h1 className="brand-title">Cacti Crafted</h1>
+        </div>
+        </>
+    )
+        
+    ;
+}
